6b35fc35bfefe765aed59f9ac23af5aab622f452
Adrian de Lomana <adrianlopezgarciadelomana@gmail.com>
2017-06-13 14:32:59 -0700

    re-arranging dir structure

--- 2017/src/requisites.r
+++ 2017/src/requisites.r
@@ -4,5 +4,5 @@ library(Rtsne) # implementation of t-SNE algorithm
 library(RColorBrewer) # library to access easily multiple colors for plotting
 library(scatterplot3d) # library for static 3D plotting
 library(rgl) # 3D Visualization Using OpenGL. If it fails in your Mac OS X, make sure you have X11 installed (https://www.xquartz.org/)
-library(tictoc) # a library to profile execution time
-library(cluster)
+library(tictoc) # library to profile execution time
+library(cluster) # library for clustering 
